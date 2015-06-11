5c3cd3078dc3e09ab45d1d793de8bec01e031ad2
Asular <ab6428890@163.com>
2015-06-11 10:04:58 +0800

    11
    123

---
+++ js/data.js
@@ -0,0 +1,205 @@
+/**
+ * Created by Administrator on 2015/6/10.
+ */
+var danmukuData= [
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'5',
+        'txt':'afakfafakgkaglajgajf'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'20',
+        'txt':'afafafafasfafoooooooooo'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'2',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'6',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'9',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'12',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'33',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'62',
+        'txt':'what is this,it look so cool!'
+    },
+    {
+        'time':'72',
+        'txt':'what is this,it look so cool!'
+    }
+];
